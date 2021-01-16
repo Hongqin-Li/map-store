b15107aca8fad0737a20540360433be2d15d5fa7
Hongqin-Li <Hongqin-Li@users.noreply.github.com>
2021-01-16 15:37:06 +0000

    deploy: eeb318f7c087eebc5bbeafd967dedde95dfc4a44

--- implementors/core/marker/trait.Sync.js
+++ implementors/core/marker/trait.Sync.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["map_store"] = [{"text":"impl Sync for BatchWriter","synthetic":true,"types":[]},{"text":"impl&lt;V, O&gt; Sync for MapStore&lt;V, O&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;O: Sync,<br>&nbsp;&nbsp;&nbsp;&nbsp;V: Sync,&nbsp;</span>","synthetic":true,"types":[]}];
+implementors["map_store"] = [{"text":"impl&lt;V, O&gt; Sync for MapStore&lt;V, O&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;O: Sync,<br>&nbsp;&nbsp;&nbsp;&nbsp;V: Sync,&nbsp;</span>","synthetic":true,"types":[]},{"text":"impl Sync for BatchWriter","synthetic":true,"types":[]}];
 implementors["topkstr"] = [{"text":"impl Sync for Generator","synthetic":true,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/marker/trait.Unpin.js
+++ implementors/core/marker/trait.Unpin.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["map_store"] = [{"text":"impl Unpin for BatchWriter","synthetic":true,"types":[]},{"text":"impl&lt;V, O&gt; Unpin for MapStore&lt;V, O&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;O: Unpin,<br>&nbsp;&nbsp;&nbsp;&nbsp;V: Unpin,&nbsp;</span>","synthetic":true,"types":[]}];
+implementors["map_store"] = [{"text":"impl&lt;V, O&gt; Unpin for MapStore&lt;V, O&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;O: Unpin,<br>&nbsp;&nbsp;&nbsp;&nbsp;V: Unpin,&nbsp;</span>","synthetic":true,"types":[]},{"text":"impl Unpin for BatchWriter","synthetic":true,"types":[]}];
 implementors["topkstr"] = [{"text":"impl Unpin for Generator","synthetic":true,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
