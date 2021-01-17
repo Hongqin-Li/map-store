868753d946499ae39e1c3caa46a7d822ce8def49
Hongqin-Li <Hongqin-Li@users.noreply.github.com>
2021-01-17 08:30:48 +0000

    deploy: 9a5449429956d597486a8253883ee3ecc6126826

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["map_store"] = [{"text":"impl Drop for BatchWriter","synthetic":false,"types":[]}];
+implementors["map_store"] = [{"text":"impl Drop for BatchWriter","synthetic":false,"types":[]},{"text":"impl&lt;V, O&gt; Drop for MapStore&lt;V, O&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
